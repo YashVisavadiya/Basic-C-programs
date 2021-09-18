3b4fbdccf74064d15a2877c1117aa396efb4bb9a
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2021-09-18 20:10:42 +0530

    commit

---
+++ Day_Switch.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n;
+	printf("Enter Week Number (1-7) : ");
+	scanf("%d",&n);
+	
+	switch (n)
+	{
+		case 1 :
+			printf("\nMonday");
+			break;
+		case 2 :
+			printf("\nTuesday");
+			break;
+		case 3 :
+			printf("\nWednesday");
+			break;
+		case 4 :
+			printf("\nThursday");
+			break;
+		case 5 :
+			printf("\nFriday");
+			break;
+		case 6 :
+			printf("\nSaturday");
+			break;
+		case 7 :
+			printf("\nSunday");
+			break;
+		default :
+			printf("\nInvalid Input ! Please Enter Number Between 1-7 ");
+			break;
+	}
+}
