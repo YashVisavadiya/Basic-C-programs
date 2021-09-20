f342822a367f6597b254d49ce7169e02df6e3815
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2021-09-20 22:59:55 +0530

    commit

---
+++ Feet_to_Inch.c
@@ -0,0 +1,19 @@
+/*   To convert Feet to Inch  */
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b;
+	
+
+	
+	printf("Input Feet ( in Integer ) : ");
+	scanf("%d",&a);
+	b=a*12;
+	printf("Given Feet in Inch is Given by %d",b);
+	getch();
+}
+
+
