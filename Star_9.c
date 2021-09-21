b40abebb6c963577e649c1643e1cffc3c2a116ac
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2021-09-21 23:16:49 +0530

    commit

---
+++ Star_9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,j,k;
+	
+	for(i=1;i<=4;i++)
+	{
+		k=65;
+		for(j=1;j<=7;j++)
+		{
+			if( j <= 5-i || j >= 3 + i )
+				printf("%c",k);
+				
+			else
+				printf(" ");
+				
+			j<4? k++:k--;
+		}
+		printf("\n");
+	}
+}
