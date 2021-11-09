39ac48fc04a640748382d3ac1e3937ca0ad8cb18
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2021-11-09 23:34:39 +0530

    commit

---
+++ average2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	int a,b;
+	printf("Enter a :");
+	scanf("%d",&a);
+	printf("Enter b :");
+	scanf("%d",&b);
+	float average=(a+b)/2;
+	printf("Average of %d and %d is %f",a,b,average);
+}
