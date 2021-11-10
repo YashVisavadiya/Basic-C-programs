6f2dfce464fa8a9435c67605bcda378e50ea2c6a
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2021-11-10 21:45:44 +0530

    commit

---
+++ areaofTriangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+	float height,base;
+	printf("Enter Height : ");
+	scanf("%f",&height);
+	printf("Enter Base : ");
+	scanf("%f",&base);
+	printf("Area of a Triangle is %f",((height*base)/2));
+}
