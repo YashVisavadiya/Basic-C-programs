533481cc0956d9e28d9996dbb5ef4018078809a4
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2021-11-13 15:41:52 +0530

    commit

---
+++ sign2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+    int num;
+    printf("Enter a Number to check Positive or Negative : ");
+    scanf("%d",&num);
+    if(num>=0)
+    {
+        printf("\nPositive ");
+    }
+    if(num<0)
+    {
+        printf("\nNegative");
+    }
+}
